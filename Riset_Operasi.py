655b2a9c2c2ed91c9d9a97df616aae67b47faed5
marde12345 <mardefasma123up@gmail.com>
2018-10-26 17:10:44 +0700

    Matrix selesai

--- Riset_Operasi.py
+++ Riset_Operasi.py
@@ -1,29 +1,40 @@
 import os
 
+def create_city(row,col):
+	listtujuan = []
+	listpabrik = []
+	num = 65
+	for i in range(col):
+		listtujuan.append(num)
+		num = num + 1
+	for i in range(row):
+		listpabrik.append(num)
+		num = num + 1
+	return listtujuan,listpabrik
+
 def create_table(row,col):
 	list2D = []
+	listtujuan,listpabrik = create_city(row,col)
 
 	for i in range(row):
 		list1D =[]
 		for j in range(col):
 			list1D.append(int(input("Masukkan nilai : ")))
 		list2D.append(list1D)
-		view_table(row,col,list2D)
+		view_table(row,col,list2D,listtujuan,listpabrik)
 		if input("Apakah benar matrix seperti diatas?(y/n) : ") != "y" :
 			return create_table(row,col)
 	return list2D
 
-def view_table(row,col,list2D):
+def view_table(row,col,list2D,listtujuan,listpabrik):
 	os.system('cls')
-	num = 65
-	for i in range(col):
-		print ( "\t%c" % (num),end="")
-		num = num + 1
+	print ("Pb \ Tj\t",end="")
+	for i in listtujuan:
+		print ( chr(i) ,end="\t")
 	print()
 
 	for i in list2D:
-		print("%c" % (num),end="\t")
-		num = num + 1
+		print(chr(listpabrik[list2D.index(i)]),end="\t")
 		for j in i:
 			print(j,end="\t")
 		print()
