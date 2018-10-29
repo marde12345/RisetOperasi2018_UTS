17f0eda1b68f5d8179d1e40170964e6a83731523
marde12345 <mardefasma123up@gmail.com>
2018-10-29 22:04:25 +0700

    VAM, kurang 1 None

--- Riset_Operasi.py
+++ Riset_Operasi.py
@@ -1,5 +1,6 @@
 import os
 import numpy as np
+import sys
 
 def nwc(list2D,supply,demand):
 	total = 0
@@ -28,10 +29,7 @@ def nwc(list2D,supply,demand):
 def lc(list2D,supply,demand):
 	total = 0
 	while(sum(supply)>0 and sum(demand)>0):
-		print(demand,supply)
-		print(list2D)
 		xy = np.argwhere(list2D == np.min(list2D)).tolist()[0]
-		print(xy)
 		x = xy[1]
 		y = xy[0]
 		
@@ -43,6 +41,80 @@ def lc(list2D,supply,demand):
 		
 	return total
 
+def vam(list2D,supply,demand):
+	ix = 0
+	iy = 0
+	total = 0
+	alist = []
+	blist = []
+	
+	while (sum(supply)>0):
+		alist = []
+		blist = []
+		rl = []
+		#alist supply
+		for row in list2D:
+			ad = sorted(row,key=lambda x: (x is None, x))
+			if set(row) != set([None]) and ad[1] != None:
+				a1 = ad[0]
+				a2 = ad[1]
+				alist.append(int(abs(a1-a2)))
+			elif set(row) != set([None]) and ad[1] == None:
+				alist.append(ad[0])
+			else:
+				alist.append(0)
+
+		#blist demand
+		for i in range(len(demand)):
+			da = sorted(listcol(list2D,i),key=lambda x: (x is None, x))
+			if set(xx[i] for xx in list2D) != set([None]) and da[1] != None:
+				blist.append(abs(da[0]-da[1]))
+			elif set(xx[i] for xx in list2D) != set([None]) and da[1] == None:
+				blist.append(da[0])
+			else:
+				blist.append(0)
+		
+		if (set(alist)==set([0]) and set(blist)==set([0])):
+			return
+		print("1",alist,set(alist),blist,set(blist))
+
+		if(max(alist)>max(blist)):
+			iy = alist.index(max(alist))
+			ix = alist.index(min(list2D[iy]))
+			print("1",ix,iy)
+		else:
+			ix = blist.index(max(blist))
+			li = listcol(list2D,ix)
+			iy = li.index(min(i for i in li if i is not None))
+			print("2",ix,iy)
+
+		sub = min(supply[iy],demand[ix])
+		total = total + (list2D[iy][ix] * sub)
+		supply[iy] = supply[iy] - sub
+		demand[ix] = demand[ix] - sub
+		if supply[iy] == 0 and demand[ix] == 0:
+			for n in range(len(demand)):
+				list2D[iy][n] = None
+			for n in range(len(supply)):
+				list2D[n][ix] = None
+		elif supply[iy] == 0:
+			for n in range(len(demand)):
+				list2D[iy][n] = None
+		elif demand[ix] == 0:
+			for n in range(len(supply)):
+				list2D[n][ix] = None
+		print(list2D)
+
+def listcol(list2D,i):
+	lo = []
+	for row in list2D:
+		lo.append(row[i])
+	return lo
+
+def get2val(list2D,row,col):
+	print("row",sorted(list2D[row])[1])
+	print("col",sorted(x[col] for x in list2D)[1])
+	print(list2D)
 
 def add_supply(row,listpabrik):
 	list = []
@@ -116,9 +188,12 @@ print(dem,sup)
 view_table(row,col,Matrix2D,listtujuan,listpabrik,sup,dem)
 
 #a = nwc(Matrix2D,sup,dem)
-#print(a)
-print("2",dem,sup)
-print(lc(Matrix2D,sup,dem))
+#print(lc(Matrix2D,sup,dem))
+#print(np.sort(Matrix2D))
+#get2val(Matrix2D,1,2)
+vam(Matrix2D,sup,dem)
+
+
 
 #x = np.argwhere(Matrix2D == np.min(Matrix2D)).tolist()[0] #y,x
 #print(x)
